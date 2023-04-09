54a974dac48627bf5e6910a6c0c5c57eca8bb169
marc <marcmodin@yahoo.com>
2023-04-09 10:56:54 +0200

    ci: build on pr, export on local

--- ci/dagger.go
+++ ci/dagger.go
@@ -10,46 +10,44 @@ import (
 	"dagger.io/dagger"
 )
 
-// Set the versions of containers used in the pipeline
+const (
+	GO_VERSION         = "1.20"
+	SYFT_VERSION       = "v0.76.0"
+	GORELEASER_VERSION = "v1.16.2"
+	APP_NAME           = "dagger-go-example-app"
+	BUILD_PATH         = "dist"
+)
+
 var (
-	goVersion         = "1.20"
-	gosyftVersion     = "v0.76.0"
-	goreleaserVersion = "v1.16.2"
-
-	// app info
-	appName      = "dagger-go-example-app"
-	appBuildPath = "dist"
-
 	err      error
 	res      string
 	is_local bool
 )
 
-func init() {
-	flag.BoolVar(&is_local, "local", false, "whether to run locally")
-	flag.Parse()
-}
-
 func main() {
 
+	// Set a global flag when running locally
+	flag.BoolVar(&is_local, "local", false, "whether to run locally [global]")
+	flag.Parse()
+
+	// Parse the first argument as the task
+	task := flag.Arg(0)
+
 	// Check if a task argument is provided
-	if len(flag.Args()) == 0 {
-		fmt.Println("Missing argument. Expected either 'pull-request' or 'release'.")
-		os.Exit(1)
+	if len(task) == 0 {
+		log.Fatalln("Missing argument. Expected either 'pull-request' or 'release'.")
 	}
 
 	// Check if the task argument is valid
-	task := flag.Arg(0)
 	if task != "pull-request" && task != "release" {
-		fmt.Println("Invalid argument. Expected either 'pull-request' or 'release'.")
-		os.Exit(1)
+		log.Fatalln("Invalid argument. Expected either 'pull-request' or 'release'.")
 	}
 
 	// Dagger client context
 	ctx := context.Background()
 
 	// Create a Dagger client
-	client, err := dagger.Connect(ctx)
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
 	if err != nil {
 		panic(err)
 	}
@@ -62,7 +60,7 @@ func main() {
 
 	log.Println("Connected to Dagger")
 
-	// Run the corresponding task.
+	// Run the corresponding task command.
 	switch task {
 	case "pull-request":
 		res, err = pullrequest(ctx, client)
@@ -82,17 +80,15 @@ func main() {
 
 // Pull Request Task: Runs tests and builds the binary
 //
-// `example: go run ci/dagger.go pull-request `
+// `example: go run ci/dagger.go [-local,-help] pull-request `
 func pullrequest(ctx context.Context, client *dagger.Client) (string, error) {
 
 	// Get the source code from host directory and exclude files
-	directory := client.Host().Directory(".", dagger.HostDirectoryOpts{
-		Exclude: []string{"dist", "vendor", ".git", "ci/*", "go.work"},
-	})
+	directory := client.Host().Directory(".")
 
 	// Create a go container with the source code mounted
 	golang := client.Container().
-		From(fmt.Sprintf("golang:%s-alpine", goVersion)).
+		From(fmt.Sprintf("golang:%s-alpine", GO_VERSION)).
 		WithMountedDirectory("/src", directory).WithWorkdir("/src").
 		WithMountedCache("/go/pkg/mod", client.CacheVolume("gomod")).
 		WithEnvVariable("CGO_ENABLED", "0")
@@ -107,92 +103,75 @@ func pullrequest(ctx context.Context, client *dagger.Client) (string, error) {
 
 	log.Println("Tests passed successfully!")
 
-	// Run go build to check if the binary compiles
-	_, err = golang.WithExec([]string{"go", "build", "-o", fmt.Sprintf("%v/%s", appBuildPath, appName), "."}).Stderr(ctx)
+	// Run gorelaser release to check if the binary compiles in all platforms
+	goreleaser := goreleaserContainer(ctx, client, directory).WithExec([]string{"release", "--snapshot", "--clean"})
+
+	// Return any errors from the goreleaser build
+	_, err = goreleaser.Stderr(ctx)
 
 	if err != nil {
 		return "", err
 	}
 
-	return "Build passed successfully!", nil
+	// Export builds to the host when running locally
+	if is_local {
+
+		// Retrieve the dist directory from the container
+		dist := goreleaser.Directory(BUILD_PATH)
+
+		// Export the dist directory when running locally
+		_, err = dist.Export(ctx, BUILD_PATH)
+
+		if err != nil {
+			return "", err
+		}
+
+		log.Printf("Exported %v to local successfully!", BUILD_PATH)
+
+	}
+
+	return "Pull-Request tasks completed successfully!", nil
 }
 
 // Release Task: Runs GoReleaser to creates a Github release
 //
-// `example: go run ci/dagger.go release flags[-l,--local]`
+// `example: go run ci/dagger.go release`
 func release(ctx context.Context, client *dagger.Client) (string, error) {
-
-	// Set the Github token from the host environment as a secret
-	token := client.SetSecret("github_token", os.Getenv("GITHUB_TOKEN"))
 
 	// Get the source code from host directory
 	directory := client.Host().Directory(".")
 
+	// Run gorelaser release to check if the binary compiles in all platforms
+	goreleaser := goreleaserContainer(ctx, client, directory).WithExec([]string{"--clean"})
+
+	// Return any errors from the goreleaser build
+	_, err = goreleaser.Stderr(ctx)
+
+	if err != nil {
+		return "", err
+	}
+
+	return "Release tasks completed successfully!", nil
+}
+
+// goreleaserContainer returns a goreleaser container with the syft binary mounted and GITHUB_TOKEN secret set
+//
+// `example: goreleaserContainer(ctx, client, directory).WithExec([]string{"build"})`
+func goreleaserContainer(ctx context.Context, client *dagger.Client, directory *dagger.Directory) *dagger.Container {
+	// Set the Github token from the host environment as a secret
+	token := client.SetSecret("github_token", os.Getenv("GITHUB_TOKEN"))
+
 	// Export the syft binary from the syft container as a file
-	syft := client.Container().From(fmt.Sprintf("anchore/syft:%s", gosyftVersion)).
+	syft := client.Container().From(fmt.Sprintf("anchore/syft:%s", SYFT_VERSION)).
 		WithMountedCache("/go/pkg/mod", client.CacheVolume("gomod")).
 		File("/syft")
 
-	// Create the GoReleaser container with the syft binary mounted
-	goreleaser := client.Container().From(fmt.Sprintf("goreleaser/goreleaser:%s", goreleaserVersion)).
+	// Run go build to check if the binary compiles
+	return client.Container().From(fmt.Sprintf("goreleaser/goreleaser:%s", GORELEASER_VERSION)).
+		WithMountedCache("/go/pkg/mod", client.CacheVolume("gomod")).
 		WithFile("/bin/syft", syft).
 		WithMountedDirectory("/src", directory).WithWorkdir("/src").
-		WithMountedCache("/go/pkg/mod", client.CacheVolume("gomod")).
 		WithEnvVariable("TINI_SUBREAPER", "true").
 		WithSecretVariable("GITHUB_TOKEN", token)
 
-	if !is_local {
-		// Run Github Release when event is push and ref is a tag
-		_, err := goreleaser.WithExec([]string{"--clean"}).Stderr(ctx)
-
-		if err != nil {
-			return "", err
-		}
-
-		log.Println("Released successfully!")
-
-	} else {
-		// If --local is set, run local release with snapshot and publish container to registry for testing
-		local := goreleaser.WithExec([]string{"--snapshot", "--clean"})
-
-		_, err := local.Stderr(ctx)
-
-		if err != nil {
-			return "", err
-		}
-
-		log.Println("Build with snapshot completed successfully!")
-
-		// Retrieve the built linux binary file from the container
-		dist := local.Directory(appBuildPath)
-
-		// Export the dist directory when running locally
-		_, err = dist.Export(ctx, appBuildPath)
-
-		if err != nil {
-			return "", err
-		}
-
-		log.Printf("Exported %v to local successfully!", appBuildPath)
-
-		// Retrieve the built linux binary file from the container
-		binary := dist.File(fmt.Sprintf("%s_linux_amd64", appName))
-
-		// Publish
-		// package the binary into a alpine container for publishing
-		publish := client.Container().From("alpine:latest").
-			WithFile(fmt.Sprintf("/bin/%v", appName), binary).
-			WithWorkdir("/bin").
-			WithEntrypoint([]string{fmt.Sprintf("/bin/%v", appName)})
-
-		container_uri, err := publish.Publish(ctx, fmt.Sprintf("ttl.sh/%s:5m", appName))
-
-		if err != nil {
-			return "", err
-		}
-
-		log.Printf("Published to registry successfully! \n %s ", container_uri)
-	}
-
-	return "Release completed successfully!", nil
 }
